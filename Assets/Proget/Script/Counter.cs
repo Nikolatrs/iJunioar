88eec3fd9db42bb5f80dc2483c180bad9a460bd0
Nikolatrs <sivohnikola97@gmail.com>
2025-03-13 23:19:54 +0300

    change delay

--- Assets/Proget/Script/Counter.cs
+++ Assets/Proget/Script/Counter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
-    [SerializeField] private float _delay = 0.1f;
+    [SerializeField] private float _delay;
 
     private bool _isCountTrigger = true;
     private int _count = 0;
